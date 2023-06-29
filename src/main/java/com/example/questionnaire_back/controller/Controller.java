dcce6b44fa0fdbf327eb2f374518ccf7259e76e9
Jyun <taiwanman1212@gmail.com>
2023-06-29 16:49:08 +0800

    plus @RequestBody!!!!!

--- src/main/java/com/example/questionnaire_back/controller/Controller.java
+++ src/main/java/com/example/questionnaire_back/controller/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.questionnaire_back.service.ifs.QuestionnaireService;
@@ -36,14 +37,14 @@ public class Controller {
 
 	// 新增問卷
 	@PostMapping(value = "add_questionnaire")
-	public QuestionnaireResponse addQuestionnaire(QuestionnaireRequest request) {
+	public QuestionnaireResponse addQuestionnaire(@RequestBody QuestionnaireRequest request) {
 		return questionnaireService.addQuestionnaire(request.getTitle(), request.getDescription(),
 				request.getStartTime(), request.getEndTime());
 	};
 
 	// 修改問卷
 	@PostMapping(value = "revise_questionnaire")
-	public QuestionnaireResponse reviseQuestionnaire(QuestionnaireRequest request) {
+	public QuestionnaireResponse reviseQuestionnaire(@RequestBody QuestionnaireRequest request) {
 		return questionnaireService.reviseQuestionnaire(request.getSerialNumber(), request.getTitle(),
 				request.getDescription(), request.getStatus(), request.getStartTime(), request.getEndTime(),
 				request.getQuestionAmount());
@@ -51,38 +52,38 @@ public class Controller {
 
 	// 刪除問卷
 	@DeleteMapping(value = "delete_questionnaire")
-	public QuestionnaireResponse deleteQuestionnaire(QuestionnaireRequest request) {
+	public QuestionnaireResponse deleteQuestionnaire(@RequestBody QuestionnaireRequest request) {
 		return questionnaireService.deleteQuestionnaire(request.getSerialNumber());
 	};
 
 	// 新增/修改問題集
 	@PostMapping(value = "add_questions")
-	public QuestionsResponse addQuestions(QuestionsRequest request) {
+	public QuestionsResponse addQuestions(@RequestBody QuestionsRequest request) {
 		return questionsService.addQuestions(request.getQuestionsList());
 	};
 
 	// 顯示該問卷問題集
 	@PostMapping(value = "show_questions")
-	public QuestionsResponse showQuestions(QuestionsRequest request) {
+	public QuestionsResponse showQuestions(@RequestBody QuestionsRequest request) {
 		return questionsService.showQuestions(request.getQnNumber());
 	};
 
 	// 刪除問題集
 	@DeleteMapping(value = "delete_questions")
-	public QuestionsResponse deleteQuestions(QuestionsRequest request) {
+	public QuestionsResponse deleteQuestions(@RequestBody QuestionsRequest request) {
 		return questionsService.deleteQuestions(request.getSerialNumberList());
 	};
 
 	// 新增使用者回答
 	@PostMapping(value = "add_records")
-	public RecordsResponse addRecords(RecordsRequest request) {
+	public RecordsResponse addRecords(@RequestBody RecordsRequest request) {
 		return recordsService.addRecords(request.getName(), request.getPhoneNumber(), request.getEmail(),
 				request.getAge(), request.getQnNumber(), request.getAnswers());
 	};
 
 	// 查詢使用者填答
 	@PostMapping(value = "find_records")
-	public RecordsResponse findRecords(RecordsRequest request) {
+	public RecordsResponse findRecords(@RequestBody RecordsRequest request) {
 		return recordsService.findRecords(request.getQnNumber());
 	};
 }
--- src/test/java/com/example/questionnaire_back/QuestionnaireBackApplicationTests.java
+++ src/test/java/com/example/questionnaire_back/QuestionnaireBackApplicationTests.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 import com.example.questionnaire_back.constants.RtnCode;
+import com.example.questionnaire_back.entity.Questionnaire;
 import com.example.questionnaire_back.entity.Questions;
+import com.example.questionnaire_back.entity.Records;
 import com.example.questionnaire_back.repository.QuestionnaireDao;
 import com.example.questionnaire_back.repository.QuestionsDao;
 import com.example.questionnaire_back.repository.RecordsDao;
@@ -42,35 +44,35 @@ public class QuestionnaireBackApplicationTests {
 	@Autowired
 	private RecordsService rSer;
 
-	@BeforeEach
-	private void BeforeEach() {
-//		qnDao.saveAll(new ArrayList<>(Arrays.asList(
-//				new Questionnaire("title1", "des", "未開放", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
-//						LocalDateTime.of(2023, 7, 31, 12, 0, 0), 10),
-//				new Questionnaire("title2", "des", "未開放", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
-//						LocalDateTime.of(2023, 7, 31, 12, 0, 0), 10))));
-//		qDao.saveAll(new ArrayList<>(Arrays.asList(new Questions(1, 1, "單選方塊", true, "Q1", "選項1, 選項2, 選項3, 選項4"),
-//				new Questions(1, 2, "單選方塊", true, "Q2", "選項1, 選項2, 選項3, 選項4"),
-//				new Questions(1, 3, "單選方塊", true, "Q3", "選項1, 選項2, 選項3, 選項4"),
-//				new Questions(2, 1, "單選方塊", true, "Q1", "選項1, 選項2, 選項3, 選項4"),
-//				new Questions(2, 2, "單選方塊", true, "Q2", "選項1, 選項2, 選項3, 選項4"),
-//				new Questions(2, 3, "單選方塊", true, "Q3", "選項1, 選項2, 選項3, 選項4"))));
-//		rDao.saveAll(new ArrayList<>(Arrays.asList(
-//				new Records("name1", "0911111111", "123@gmail.com", 12, 1, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
-//						"1,2,3"),
-//				new Records("name2", "0911111112", "124@gmail.com", 18, 1, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
-//						"1,2,3"),
-//				new Records("name3", "0911111113", "125@gmail.com", 20, 1, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
-//						"1,2,3"),
-//				new Records("name4", "0911111114", "126@gmail.com", 24, 2, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
-//						"1,2,3"),
-//				new Records("name5", "0911111115", "127@gmail.com", 30, 2, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
-//						"1,2,3"))));
+	@BeforeAll
+	private void BeforeAll() {
+		qnDao.saveAll(new ArrayList<>(Arrays.asList(
+				new Questionnaire("title1", "des", "未開放", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
+						LocalDateTime.of(2023, 7, 31, 12, 0, 0), 10),
+				new Questionnaire("title2", "des", "未開放", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
+						LocalDateTime.of(2023, 7, 31, 12, 0, 0), 10))));
+		qDao.saveAll(new ArrayList<>(Arrays.asList(new Questions(1, 1, "單選方塊", true, "Q1", "選項1, 選項2, 選項3, 選項4"),
+				new Questions(1, 2, "單選方塊", true, "Q2", "選項1, 選項2, 選項3, 選項4"),
+				new Questions(1, 3, "單選方塊", true, "Q3", "選項1, 選項2, 選項3, 選項4"),
+				new Questions(2, 1, "單選方塊", true, "Q1", "選項1, 選項2, 選項3, 選項4"),
+				new Questions(2, 2, "單選方塊", true, "Q2", "選項1, 選項2, 選項3, 選項4"),
+				new Questions(2, 3, "單選方塊", true, "Q3", "選項1, 選項2, 選項3, 選項4"))));
+		rDao.saveAll(new ArrayList<>(Arrays.asList(
+				new Records("name1", "0911111111", "123@gmail.com", 12, 1, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
+						"1,2,3"),
+				new Records("name2", "0911111112", "124@gmail.com", 18, 1, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
+						"1,2,3"),
+				new Records("name3", "0911111113", "125@gmail.com", 20, 1, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
+						"1,2,3"),
+				new Records("name4", "0911111114", "126@gmail.com", 24, 2, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
+						"1,2,3"),
+				new Records("name5", "0911111115", "127@gmail.com", 30, 2, LocalDateTime.of(2023, 7, 10, 16, 0, 0),
+						"1,2,3"))));
 	}
 
 	@Test
 	public void insertQuestionnaireTest() {
-		Assert.isTrue(qnDao.insertQuestionnaire("title3", "des", "未開放", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
+		Assert.isTrue(qnDao.insertQuestionnaire("title2", "des", "未開放", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
 				LocalDateTime.of(2023, 7, 31, 12, 0, 0)) == 1, RtnCode.TEST1_ERROR.getMessage());
 	}
 
@@ -85,7 +87,7 @@ public class QuestionnaireBackApplicationTests {
 	@Test
 	public void deleteQuestionnaireTest() {
 		Assert.isTrue(qnDao.deleteQuestionnaire(999) == 0, RtnCode.TEST1_ERROR.getMessage());
-		Assert.isTrue(qnDao.deleteQuestionnaire(2) == 1, RtnCode.TEST2_ERROR.getMessage());
+		Assert.isTrue(qnDao.deleteQuestionnaire(4) == 1, RtnCode.TEST2_ERROR.getMessage());
 	}
 
 	@Test
@@ -98,7 +100,7 @@ public class QuestionnaireBackApplicationTests {
 		Assert.isTrue(qnSer.addQuestionnaire("test", "des", LocalDateTime.of(2023, 6, 10, 12, 0, 0), null).getMessage()
 				.equals(RtnCode.CANNOT_EMPTY.getMessage()), RtnCode.TEST3_ERROR.getMessage());
 		Assert.isTrue(
-				qnSer.addQuestionnaire("title3", "des", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
+				qnSer.addQuestionnaire("title1", "des", LocalDateTime.of(2023, 6, 30, 12, 0, 0),
 						LocalDateTime.of(2023, 7, 31, 12, 0, 0)).getMessage().equals(RtnCode.INCORRECT.getMessage()),
 				RtnCode.TEST4_ERROR.getMessage());
 		Assert.isTrue(
@@ -172,13 +174,13 @@ public class QuestionnaireBackApplicationTests {
 				RtnCode.TEST1_ERROR.getMessage());
 		Assert.isTrue(qSer.deleteQuestions(new ArrayList<>(Arrays.asList(0, 1, 2))).getMessgage()
 				.equals(RtnCode.INCORRECT.getMessage()), RtnCode.TEST2_ERROR.getMessage());
-		Assert.isTrue(qSer.deleteQuestions(new ArrayList<>(Arrays.asList(230))).getMessgage()
+		Assert.isTrue(qSer.deleteQuestions(new ArrayList<>(Arrays.asList(3))).getMessgage()
 				.equals(RtnCode.SUCCESS.getMessage()), RtnCode.TEST3_ERROR.getMessage());
 	}
 
 	@Test
 	public void insertRecordsTest() {
-		Assert.isTrue(rDao.insertRecords("name6", "0911111116", "128@gmail.com", 30, 2,
+		Assert.isTrue(rDao.insertRecords("name11", "0911111116", "128@gmail.com", 30, 2,
 				LocalDateTime.of(2023, 7, 10, 16, 0, 0), "1,2,3") == 1, RtnCode.TEST1_ERROR.getMessage());
 	}
 
@@ -198,7 +200,7 @@ public class QuestionnaireBackApplicationTests {
 				.equals(RtnCode.CANNOT_EMPTY.getMessage()), RtnCode.TEST3_ERROR.getMessage());
 		Assert.isTrue(rSer.addRecords("name6", "0911111116", "128@gmail.com", 30, 2, "1,2,3").getMessage()
 				.equals(RtnCode.INCORRECT.getMessage()), RtnCode.TEST4_ERROR.getMessage());
-		Assert.isTrue(rSer.addRecords("name7", "0911111117", "129@gmail.com", 30, 2, "1,2,3").getMessage()
+		Assert.isTrue(rSer.addRecords("name17", "0911111117", "129@gmail.com", 30, 2, "1,2,3").getMessage()
 				.equals(RtnCode.SUCCESS.getMessage()), RtnCode.TEST5_ERROR.getMessage());
 	}
 
